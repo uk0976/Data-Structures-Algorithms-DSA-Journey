eef8f8ec8038e1cdf99d72673c26119862b02810
Umer Khan <142109802+uk0976@users.noreply.github.com>
2026-07-16 00:29:51 +0530

    Time: 7 ms (78.11%) | Memory: 12.4 MB (17.28%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+
+        while x > reversed_half:
+            digit = x % 10
+            reversed_half = reversed_half * 10 + digit
+            x //= 10
+
+        return x == reversed_half or x == reversed_half // 10
+        
