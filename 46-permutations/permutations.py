fdfd82ab3b89e358395b198671420f866a71919d
Umer Khan <142109802+uk0976@users.noreply.github.com>
2026-08-11 23:47:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (24.15%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def permute(self, nums):
+        result = []
+
+        def backtrack(start):
+            if start == len(nums):
+                result.append(nums[:])
+                return
+
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+
+                backtrack(start + 1)
+
+                nums[start], nums[i] = nums[i], nums[start]
+
+        backtrack(0)
+        return result
